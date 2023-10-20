39e714d26edcc988bc172ee30de6eb005096e83c
max_l <max_lecona@hotmail.com>
2023-10-20 11:12:08 -0600

    Aplicacion Comedor

--- app/src/main/java/mx/rmr/menuhamburguesaadmin/ui/view/MainActivity.kt
+++ app/src/main/java/mx/rmr/menuhamburguesaadmin/ui/view/MainActivity.kt
@@ -12,8 +12,11 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.ui.NavigationUI
 import mx.rmr.menuhamburguesaadmin.R
 import mx.rmr.menuhamburguesaadmin.databinding.ActivityMainBinding
+import mx.rmr.menuhamburguesaadmin.databinding.FragmentPrincipalBinding
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
@@ -29,24 +32,44 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-//        binding.appBarMain.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_calificaciones, R.id.nav_escanearqr, R.id.nav_registrarnuevousuario,
                 R.id.nav_inventario, R.id.nav_menu, R.id.nav_reportes
             ), drawerLayout
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Agrega este bloque de código
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_logout -> {
+                    println("Se salio we")
+                    restartApp()
+                    true
+                }
+                else -> {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    // Delega el manejo de los demás elementos del menú a la implementación predeterminada
+                    return@setNavigationItemSelectedListener NavigationUI.
+                    onNavDestinationSelected(menuItem, navController) ||
+                            super.onOptionsItemSelected(menuItem)
+                }
+            }
+        }
     }
+
+    private fun restartApp() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -58,8 +81,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-
-
 
 }
